b359061f24ceb58c26655ecbdc0bd6d5b48f5ec1
Malik Ahad <malikahadofficial@gmail.com>
2026-08-08 20:41:39 -0700

    Add listing schema and seed data

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+// calling the main function
+main()
+    .then(() => {
+        console.log("connected to DB")
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    //first we delete all the data in the database
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Data was initialized");
+}
+
+initDB();
