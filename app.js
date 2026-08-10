ccf3ebb31eaea45a477f178f12694c96a33c3559
Malik Ahad <malikahadofficial@gmail.com>
2026-08-09 20:24:57 -0700

    Add listing details view

--- app.js
+++ app.js
@@ -21,15 +21,24 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root!");
 });
 
+// Index route
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
 });
+
+// Show route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+})
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
